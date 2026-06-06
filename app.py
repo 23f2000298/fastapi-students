ed31dfa806c9ff0b4d7ee580b850afc165e8d92a
Joydev Mandal <23f2000298@ds.study.iitm.ac.in>
2026-06-06 09:00:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
+df = pd.read_csv("students.csv")
+
+@app.get("/api")
+async def get_students(class_: list[str] = Query(default=None, alias="class")):
+    data = df
+    if class_:
+        data = df[df["class"].isin(class_)]
+    return {"students": data.to_dict(orient="records")}
